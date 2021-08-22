2c37db20c97f40d0410141b154395e8d139c922b
Francesco Gatti <gattifrancesco@hotmail.it>
2021-08-23 00:00:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# git forest
+utilty to easly manage git subtrees
+
+.gitforest file example:
+```
+tkCommon:
+    prefix: "tkCommmon"
+    url: "gitlab@git.hipert.unimore.it:tk/core/tkCommon.git"
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='git-forest',  
+     version='0.1',
+     scripts=['git-forest'] ,
+     author="Francesco Gatti",
+     author_email="gattifrancesco@hotmail.it",
+     description="manage subtree",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/ceccocats/git-forest",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
